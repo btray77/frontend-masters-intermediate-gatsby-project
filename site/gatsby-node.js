edf12a6480b9fca82327ae9b15726147c90150eb
Brad <btray77@gmail.com>
2021-08-21 03:23:33 -0500

    push 2

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -1,3 +1,47 @@
+const authors = require('./src/data/authors.json');
+const books = require('./src/data/books.json');
+
+exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
+  const { createNode, createTypes } = actions;
+
+  createTypes(`
+    type Author implements Node {
+        books: [Book!]! @link(from: "slug" by: "author.slug")
+    }
+
+    type Book implements Node {
+        author: Author! @link(from: "author" by: "slug")
+    }
+    `);
+
+  authors.forEach((author) => {
+    createNode({
+      ...author,
+      id: createNodeId(`author-${author.slug}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Author',
+        content: JSON.stringify(author),
+        contentDigest: createContentDigest(author),
+      },
+    });
+  });
+
+  books.forEach((book) => {
+    createNode({
+      ...book,
+      id: createNodeId(`book-${book.isbn}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Book',
+        content: JSON.stringify(book),
+        contentDigest: createContentDigest(book),
+      },
+    });
+  });
+};
 exports.createPages = ({ actions }) => {
   const { createPage } = actions;
 
